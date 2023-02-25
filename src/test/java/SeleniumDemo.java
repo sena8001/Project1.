c4425809d83e5bc7bab97b316e1be63eae0fde79
John DoeSena <john@doe.orgsena8001s@gmail.com>
2023-02-25 05:23:40 -0500

    fi

---
+++ src/test/java/SeleniumDemo.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class SeleniumDemo {
+
+    public static void main(String[] args) {
+
+        WebDriver driver=new EdgeDriver();
+        driver.get("https://www.google.com/ ");
+
+        WebElement searchBox=  driver.findElement(By.name("q"));
+      
+        
+        String expect="ChatGPT";
+        searchBox.sendKeys(expect, Keys.ENTER);
+
+        String CURRENTtITLE=driver.getTitle();
+
+if (CURRENTtITLE.contains(expect+"Dsfsdfvds")){
+    System.out.println("passed");
+
+}else{
+    System.out.println("failed" + CURRENTtITLE);
+}
+
+    }
+}
---
+++ src/test/java/Tst.java
@@ -0,0 +1,27 @@
+import com.github.javafaker.Faker;
+
+
+public class Tst {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+        Faker faker=new Faker();
+        System.out.println(faker.business().creditCardNumber());
+        System.out.println(faker.address().firstName());
+        System.out.println(faker.address().lastName());
+        System.out.println(faker.address().streetAddress());
+
+
+
+
+
+
+
+
+    }
+}
